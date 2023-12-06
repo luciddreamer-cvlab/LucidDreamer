713ebd2b05fdc118b114ec700bc337f4258a9e9d
Jaerin Lee <jarin.lee@gmail.com>
2023-12-07 00:04:16 +0900

    fixed the link

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ with gr.Blocks(css=css) as demo:
                 </a>
                 &nbsp;
                 <a href='https://twitter.com/_ironjr_'>
-                    <img src='https://img.shields.io/twitter/url?url=https%3A%2F%2Ftwitter.com%2F_ironjr_'>
+                    <img src='https://img.shields.io/twitter/url?label=_ironjr_&url=https%3A%2F%2Ftwitter.com%2F_ironjr_'>
                 </a>
             </div>
         </div>
